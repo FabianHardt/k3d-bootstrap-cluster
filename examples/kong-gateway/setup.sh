0f35130c0626858feef509e660d60002a940cfff
Fabian Hardt <fabian.hardt@opitz-consulting.com>
2022-11-09 10:44:46 +0100

    WIP: Improve setup.sh, README

--- examples/kong-gateway/setup.sh
+++ examples/kong-gateway/setup.sh
@@ -6,6 +6,23 @@ source helpers.sh
 helm repo add kong https://charts.konghq.com
 helm repo update
 
+# include Hashicorp Vault setup first
+VAULT_EXISTS=$(kubectl get ns vault || echo "false")
+
+if [ "$VAULT_EXISTS" == "false" ]
+then
+cd ../vault/
+bash setup.sh
+else
+echo "Skipping vault deployment. Already there."
+fi
+
+# Remove NGINX Ingress - replace with Kong Ingress
+kubectl delete -f ../../manifests/nginx-helm.yaml || true
+kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission || true
+
+cd ../kong-gateway/
+
 # Install Kong Controlplane
 installControlPlane
 
@@ -14,3 +31,5 @@ installDataPlane
 
 # Install Kong Ingress controller
 installIngressController
+
+kubectl apply -n demo -f httpbin-ingress-kong.yaml
