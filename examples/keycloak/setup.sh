5614089e96da349d6f722a942ec9029055729ed5
Fabian Hardt <fabian.hardt@opitz-consulting.com>
2022-12-08 23:31:27 +0100

    Fix namespace

--- examples/keycloak/setup.sh
+++ examples/keycloak/setup.sh
@@ -2,5 +2,5 @@
 set -o errexit
 
 kubectl create ns keycloak || true
-kubectl create configmap keycloak-config-realm --from-file=kong-realm.json=kong-realm.json || true
+kubectl create configmap -n keycloak keycloak-config-realm --from-file=kong-realm.json=kong-realm.json || true
 helm upgrade --install keycloak bitnami/keycloak --create-namespace --namespace keycloak --values values.yaml
