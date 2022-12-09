6f1aede74f85a18b8397f388ac44b56f0444c012
Philipp Kürsten <philipp.kuersten@opitz-consulting.com>
2022-12-09 08:50:41 +0100

    Added ingress-resources and updated setup.sh

--- examples/keycloak/setup.sh
+++ examples/keycloak/setup.sh
@@ -3,4 +3,4 @@ set -o errexit
 
 kubectl create ns keycloak || true
 kubectl create configmap -n keycloak keycloak-config-realm --from-file=kong-realm.json=kong-realm.json || true
-helm upgrade --install keycloak bitnami/keycloak --create-namespace --namespace keycloak --values values.yaml
+helm upgrade --install keycloak bitnami/keycloak --namespace keycloak --values values.yaml
