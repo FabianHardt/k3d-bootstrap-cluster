384c02fe9ba6da85c32567378a5b59721fde9b6f
Christian Kaubisch <christiankaubisch@gmail.com>
2022-12-19 09:31:01 +0100

    Added automatic Kong install

--- examples/kuma-mesh/setup.sh
+++ examples/kuma-mesh/setup.sh
@@ -1,39 +1,21 @@
 #!/bin/bash
 set -o errexit
 
+source helpers.sh
+
 helm repo add kuma https://kumahq.github.io/charts
 helm repo update
 
-helm upgrade --install kuma kuma/kuma \
-    --values kuma-cp-values.yaml \
-    --namespace kuma-cp --create-namespace
+installKumaStandalone
 
-echo "
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: kuma-gui
-  namespace: kuma-cp
-  annotations:
-    nginx.ingress.kubernetes.io/app-root: '/gui'
-    ingress.kubernetes.io/ssl-redirect: 'false'
-spec:
-  ingressClassName: kong
-  rules:
-  - host: kuma-gui.example.com
-    http:
-      paths:
-      - path: /
-        pathType: Prefix
-        backend:
-          service:
-            name: kuma-control-plane
-            port:
-              number: 5681" | kubectl apply -f -
+KONG_EXISTS=$(kubectl get ns kong-cp || echo "false")
+if [ "$KONG_EXISTS" == "false" ]
+then
+cd ../kong-gateway/
+bash setup.sh
+else
+echo "Skipping kong deployment, already installed"
+fi
 
-kubectl annotate ns kong-dp kuma.io/sidecar-injection="enabled" --overwrite
-
-kubectl -n kong-dp delete po --all
-echo 'Wait for kong dataplane to start with sidecar'
-
-kubectl wait pod -n kong-dp $(kubectl -n kong-dp get pods --no-headers -o custom-columns=":metadata.name") --for condition=Available=True --timeout=300s
+cd ../kuma-mesh/
+configureMeshForKongIngress
