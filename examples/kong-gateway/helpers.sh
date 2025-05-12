356326a0b8702a6a3a1a031897a0ad0483fafb78
Sven Bernhardt <sven.bernhardt@opitz-consulting.com>
2025-05-12 10:31:03 +0200

    Changed deployment mode to Gateway discovery

--- examples/kong-gateway/helpers.sh
+++ examples/kong-gateway/helpers.sh
@@ -1,42 +1,27 @@
 #!/bin/bash
 
 LICENSE_FILE=license.json
-LICENSE_FILE_CONTENT={}
 
-installIngressController()
-{
-# Install Gateway API CRDs
-kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.1.0/standard-install.yaml
-
-# Preconditions
+installIngressController() {
+	# Preconditions
 kubectl create namespace kong || true
-
-kubectl delete secret kong-config-secret -n kong || true
-
-kubectl create secret generic kong-config-secret -n kong \
-    --from-literal=portal_session_conf='{"storage":"kong","secret":"superfhasecret","cookie_name":"portal_session","cookie_samesite":"off","cookie_secure":false}' \
-    --from-literal=admin_gui_session_conf='{"storage":"kong","secret":"superfhasecret","cookie_name":"admin_session","cookie_samesite":"off","cookie_secure":false}' \
-    --from-literal=pg_host="enterprise-postgresql.kong.svc.cluster.local" \
-    --from-literal=kong_admin_password=kong \
-    --from-literal=password=kong
 
 kubectl apply -f gateway-class.yaml
 kubectl apply -f gateway.yaml
 
 helm upgrade --install kong kong/ingress --values values.yaml --namespace kong
 
-if [ -f "$LICENSE_FILE" ]; then
-  echo "$LICENSE_FILE exists. Using it!"
-  LICENSE_FILE_CONTENT=$(cat $LICENSE_FILE)
-fi
+if [[ -f ${LICENSE_FILE} ]]; then
+echo "${LICENSE_FILE} exists. Using it!"
 
 echo "
 apiVersion: configuration.konghq.com/v1alpha1
 kind: KongLicense
 metadata:
- name: kong-license
-rawLicenseString: '$(echo $LICENSE_FILE_CONTENT)'
+  name: kong-license
+rawLicenseString: '$(cat "${LICENSE_FILE}")'
 " | kubectl apply -f -
+fi
 
 echo "Waiting for Kong Ingress Controller Pods to be ready..."
 kubectl -n kong wait --for=condition=Available=true --timeout=120s deployment/kong-gateway
--- examples/kong-gateway/setup.sh
+++ examples/kong-gateway/setup.sh
@@ -6,10 +6,12 @@ source helpers.sh
 helm repo add kong https://charts.konghq.com
 helm repo update
 
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/standard-install.yaml
+
 # include Hashicorp Vault setup first
 VAULT_EXISTS=$(kubectl get ns vault || echo "false")
 
-if [ "$VAULT_EXISTS" == "false" ]
+if [[ "${VAULT_EXISTS}" == "false" ]]
 then
 cd ../vault/
 bash setup.sh
@@ -19,23 +21,23 @@ fi
 
 # Remove NGINX Ingress - replace with Kong Ingress
 NGINX_EXISTS=$(kubectl get ns ingress-nginx || echo "false")
-if [ "$NGINX_EXISTS" == "false" ]
+if [[ "${NGINX_EXISTS}" == "false" ]]
 then
 echo "Skipping deletion of NGINX ingress..."
 else
 kubectl delete -f ../../manifests/nginx-helm.yaml || true
 kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission || true
+
+kubectl delete ingress -n demo httpbin || true
+kubectl delete ingress -n vault vault || true
 fi
 
 cd ../kong-gateway/
 
-# Install Kong Controlplane
-#installControlPlane
-
-# Install Kong Dataplane
-#installDataPlane
-
 # Install Kong Ingress controller
 installIngressController
 
+kubectl delete pod --field-selector=status.phase==Succeeded -A
+
 kubectl apply -n demo -f httproute-httpbin-svc.yaml
+kubectl apply -n kong -f httproute-kong-manager.yaml
